4dd9337bccf9ac6d09dbf774af6fd502cf75ed81
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-10-10 21:29:59 +0200

    0.1 init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Wrapper } from "./Global-styles";
+import {GoogleMap,Marker,InfoWindow,useJsApiLoader} from "@react-google-maps/api"
+
+
+const App:React.FC=()=> {
+  return (
+ <Wrapper>
+   <p>sdf</p>
+ </Wrapper>
+  );
+}
+
+export default App;
