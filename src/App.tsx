8bf2c1f71a7d03697edba5c60e5dda76755d6a80
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-10-11 17:45:14 +0200

    0.2 gmap

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, { ElementType } from "react";
 import { Wrapper } from "./Global-styles";
-import {GoogleMap,Marker,InfoWindow,useJsApiLoader} from "@react-google-maps/api"
+import { GoogleMap, Marker, InfoWindow, useJsApiLoader } from "@react-google-maps/api";
 
+const App: React.FC = () => {
+  const containerStyle = {
+    width: '1400px',
+    height: '400px'
+  };
+  
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  };
 
-const App:React.FC=()=> {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_APP!,
+  });
+
+  const mapRef = React.useRef<google.maps.Map | null>(null);
+
+  const onLoad = (map: google.maps.Map): void => {
+    mapRef.current = map;
+  };
+
+  const unMount = () => {
+    mapRef.current = null;
+  };
+
+  if (!isLoaded) {
+    return (
+      <>
+        <div>sddf</div>
+      </>
+    );
+  }
+
   return (
- <Wrapper>
-   <p>sdf</p>
- </Wrapper>
+    <Wrapper>
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}  onLoad={onLoad} onUnmount={unMount} />
+    </Wrapper>
   );
-}
+};
 
 export default App;
