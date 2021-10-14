f9617d55a4766489d1ba7e512a65f13cb846e95b
GergelyKarl <57073412+GergelyKarl@users.noreply.github.com>
2021-10-14 16:44:13 +0200

    0.4 info window

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Wrapper } from "./Global-styles";
-import { GoogleMap, Marker, InfoWindow, useJsApiLoader } from "@react-google-maps/api";
-import {useQuery} from "react-query"
+import { GoogleMap, Marker, InfoWindow, useJsApiLoader, InfoBox } from "@react-google-maps/api";
+import { useQuery } from "react-query";
+import { fetcWeather } from "./api";
 
 export type WeatherType = {
   temp: number;
@@ -9,19 +10,35 @@ export type WeatherType = {
 };
 
 export type Coordinate = {
-location:google.maps.LatLngLiteral
+  lat: number;
+  lng: number;
 };
 
+const testr = {
+  lat: 47,
+  lng: 19,
+};
 
 const App: React.FC = () => {
+  const [selectedPosition, setSelectedPosition] = useState<Coordinate>({} as Coordinate);
   const containerStyle = {
-    width: '1400px',
-    height: '400px'
+    width: "1400px",
+    height: "720px",
   };
-  
+
+  const { data, isLoading, error, status } = useQuery("fetcfromhapi", () => fetcWeather(selectedPosition), {
+    staleTime: 1000,
+    cacheTime: 300,
+  });
+
+  const clickHandler = (e: google.maps.MapMouseEvent) => {
+    setSelectedPosition({ lat: e.latLng.lat(), lng: e.latLng.lng() });
+    console.log(selectedPosition);
+  };
+
   const center = {
-    lat: -3.745,
-    lng: -38.523
+    lat: 47.49,
+    lng: 19.04,
   };
 
   const { isLoaded } = useJsApiLoader({
@@ -42,14 +59,48 @@ const App: React.FC = () => {
   if (!isLoaded) {
     return (
       <>
-        <div>sddf</div>
+        <div>...</div>
+      </>
+    );
+  }
+  if (isLoading) {
+    return (
+      <>
+        <div>loading...</div>
       </>
     );
   }
 
+  console.log(data);
+
   return (
     <Wrapper>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}  onLoad={onLoad} onUnmount={unMount} />
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onLoad={onLoad}
+        onUnmount={unMount}
+        onClick={clickHandler}
+        options={{ zoomControl: true }}
+      >
+        {selectedPosition.lat ? <Marker position={selectedPosition} /> : null}
+        {selectedPosition.lat && (
+          <InfoWindow position={selectedPosition}>
+            <div>
+              <h3>{data?.location?.city}</h3>
+              {isLoading ? (
+                <p>Loading Weather ...</p>
+              ) : (
+                <>
+                  <p>{data?.current_observation?.condition?.temperature}</p>
+                  <p>{data?.current_observation?.condition?.text} </p>
+                </>
+              )}
+            </div>
+          </InfoWindow>
+        )}
+      </GoogleMap>
     </Wrapper>
   );
 };
--- src/api.ts
+++ src/api.ts
@@ -1,18 +1,20 @@
 import { WeatherType, Coordinate } from "./App";
 export const fetcWeather = async (coordinate: Coordinate): Promise<any> => {
   const respone: any = await fetch(
-    `https://yahoo-weather5.p.rapidapi.com/weather?lat=${coordinate.location.lat}&lang=${coordinate.location.lng}&format=json&u=f`,
+    `https://yahoo-weather5.p.rapidapi.com/weather?lat=${coordinate.lat}&long=${coordinate.lng}&format=json&u=c`,
     {
       method: "GET",
       headers: {
         "x-rapidapi-host": "yahoo-weather5.p.rapidapi.com",
-        "x-rapidapi-key": process.env.REACT_APP_WEATHER_APP!,
+        "x-rapidapi-key": `${process.env.REACT_APP_WEATHER_APP!}`,
       },
     }
   );
 
   const data = await respone.json();
-  console.log(data);
-
   return data;
+  //   return {
+  //     temp: data.value.current_observation.condition.temperature,
+  //     text: data.value.current_observation.condition.text,
+  //   };
 };
